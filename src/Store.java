3ee00d59d074c727070599aaeb2f2c6f7b7eb6eb
phuctong2002 <vanphuc15092002@gmail.com>
2022-11-26 21:45:47 +0700

    task 7

---
+++ src/Store.java
@@ -0,0 +1,20 @@
+public class Store {
+    private DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[1000];
+    private int qty;
+    public void addDVD( DigitalVideoDisc dvd){
+        itemsInStore[qty] = dvd;
+        ++qty;
+    }
+
+    public void removeDVD( DigitalVideoDisc dvd){
+        for( int i = 0; i < qty; ++i){
+            if( dvd == itemsInStore[i]) {
+                for (int j = i + 1; j < qty; ++j) {
+                    itemsInStore[j - 1] = itemsInStore[j];
+                }
+                --qty;
+                return;
+            }
+        }
+    }
+}
